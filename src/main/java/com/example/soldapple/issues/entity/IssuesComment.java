c6a47e0c56542274fb444f926e6b8f26b1250507
NayoonBaek <vlfdyd57@naver.com>
2022-11-10 18:13:16 +0900

    #3 이의제기 댓글 수정(전체 조회시 회원정보 뜨지 않게 수정)

--- src/main/java/com/example/soldapple/issues/entity/IssuesComment.java
+++ src/main/java/com/example/soldapple/issues/entity/IssuesComment.java
@@ -29,12 +29,14 @@ public class IssuesComment extends TimeStamped {
     private Issues issues;
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn
     private Member member;
-
+    private String nickname;
     public IssuesComment(Issues issues, Member member, String issuesComment){
     this.issues = issues;
     this.member = member;
+    this.nickname = member.getNickname();
     this.issuesComment = issuesComment;
     }
 }
