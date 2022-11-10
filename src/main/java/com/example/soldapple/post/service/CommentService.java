0e9ddd65a0d4ed4d4f3fa5704ff1f464bb836683
machdd <jkk1637@naver.com>
2022-11-11 05:17:32 +0900

    김재경: 게시글 댓글 작성,수정,삭제에서 토큰 사용하도록 수정

--- src/main/java/com/example/soldapple/post/service/CommentService.java
+++ src/main/java/com/example/soldapple/post/service/CommentService.java
@@ -1,10 +1,13 @@
 package com.example.soldapple.post.service;
 
+import com.example.soldapple.member.entity.Member;
 import com.example.soldapple.post.dto.CommentReqDto;
+import com.example.soldapple.post.dto.CommentResponseDto;
 import com.example.soldapple.post.entity.Comment;
 import com.example.soldapple.post.entity.Post;
 import com.example.soldapple.post.repository.CommentRepository;
 import com.example.soldapple.post.repository.PostRepository;
+import com.example.soldapple.security.user.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,35 +20,35 @@ public class CommentService {
     private final PostRepository postRepository;
 
     @Transactional
-    public String commentCreate(Long postId, CommentReqDto commentReqDto) {
+    public CommentResponseDto commentCreate(Long postId, CommentReqDto commentReqDto, UserDetailsImpl userDetails) {
 
 
         Post post = postRepository.findById(postId).orElseThrow(
                 () -> new IllegalArgumentException("post not exist")
 
         );
-        Comment comment = new Comment(commentReqDto, post);
+        Comment comment = new Comment(commentReqDto, post, userDetails.getMember());
 
         commentRepository.save(comment);
 
-        return "Success";
+        return new CommentResponseDto(comment);
 
 
     }
 
     @Transactional
-    public String commentDelete(Long commentId) {
-        commentRepository.deleteById(commentId);
+    public String commentDelete(Long commentId, Member member) {
+        commentRepository.deleteByIdAndMember(commentId, member);
         return "Success";
     }
 
     @Transactional
-    public String commentEdit(Long commentId, CommentReqDto commentReqDto){
-        Comment comment = commentRepository.findById(commentId).orElseThrow(
+    public CommentResponseDto commentEdit(Long commentId, CommentReqDto commentReqDto, Member member){
+        Comment comment = commentRepository.findByIdAndMember(commentId, member).orElseThrow(
                 () -> new IllegalArgumentException("comment not exist")
 
         );
         comment.CommentEdit(commentReqDto);
-        return "Success";
+        return new CommentResponseDto(comment);
     }
 }
