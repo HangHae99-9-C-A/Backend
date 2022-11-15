9834a1c392e9beb2b9a52fdadf7d6cf6d476647d
RefinedStone <chem.en.9273@gmail.com>
2022-11-15 14:39:42 +0900

    Timezone -> korea로 변경 하였습니다

--- src/main/java/com/example/soldapple/SoldAppleApplication.java
+++ src/main/java/com/example/soldapple/SoldAppleApplication.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.Locale;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class SoldAppleApplication {
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        Locale.setDefault(Locale.KOREA);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SoldAppleApplication.class, args);
