455c23abbb60c3d79dd78f9aa0742f9cd8811967
RefinedStone <chem.en.9273@gmail.com>
2022-11-17 15:32:54 +0900

    build err 해결

--- src/test/java/com/example/soldapple/SoldAppleApplication.java
+++ src/test/java/com/example/soldapple/SoldAppleApplication.java
@@ -1,25 +1,25 @@
-package com.example.soldapple;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import javax.annotation.PostConstruct;
-import java.util.Locale;
-import java.util.TimeZone;
-
-@EnableJpaAuditing
-@SpringBootTest
-public class SoldAppleApplication {
-    @PostConstruct
-    public void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-        Locale.setDefault(Locale.KOREA);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(SoldAppleApplication.class, args);
-    }
-
-}
+//package com.example.soldapple;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//
+//import javax.annotation.PostConstruct;
+//import java.util.Locale;
+//import java.util.TimeZone;
+//
+//@EnableJpaAuditing
+//@SpringBootTest
+//public class SoldAppleApplication {
+//    @PostConstruct
+//    public void started() {
+//        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+//        Locale.setDefault(Locale.KOREA);
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(SoldAppleApplication.class, args);
+//    }
+//
+//}
