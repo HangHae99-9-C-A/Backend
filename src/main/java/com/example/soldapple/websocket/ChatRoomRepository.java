336e54ef60ba0900441d2417a042d9b4d68cbf45
jeongsoo <dnjawm19@naver.com>
2022-12-01 20:48:52 +0900

    웹소켓 병합 완료

--- src/main/java/com/example/soldapple/websocket/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.example.soldapple.websocket;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//public interface ChatRoomRepository extends JpaRepository<ChatRoom,String> {
-//    List<Chat> findChatRoomsByUserId1OrUserId2(Long userId1, Long userId2);
-//}
