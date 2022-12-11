88b822a96173047f4c97e05385b67bf1d7a0cf3d
jeongsoo <dnjawm19@naver.com>
2022-12-12 02:07:23 +0900

    웹소켓 인증 제거

--- src/main/java/com/example/soldapple/security/WebSecurityConfig.java
+++ src/main/java/com/example/soldapple/security/WebSecurityConfig.java
@@ -70,6 +70,7 @@ public class WebSecurityConfig {
 
         http.authorizeRequests()
                 .antMatchers("/member/**").permitAll()
+                .antMatchers("/ws/chat/**").permitAll()
                 .antMatchers(PERMIT_URL_ARRAY).permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
